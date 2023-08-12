a1aa6135e4a15bf0ffb0ec481ea71c4c9dce5060
unknown <pragadeeshwarad1475369@gmail.com>
2023-08-12 15:35:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+  // addition
+  function add(){
+    var a=parseInt(document.getElementById("input1").value);
+    var b= parseInt(document.getElementById("input2").value);
+    alert(a+b);
+    }
+
+    // subtration
+    function sub(){
+    var a=parseInt(document.getElementById("input1").value);
+    var b= parseInt(document.getElementById("input2").value);
+    alert(a-b);
+    }
+
+    // Multiplication
+    function mult(){
+    var a=parseInt(document.getElementById("input1").value);
+    var b= parseInt(document.getElementById("input2").value);
+    alert(a*b);
+    }
+
+    // Divide
+    function div(){
+    var a=parseInt(document.getElementById("input1").value);
+    var b= parseInt(document.getElementById("input2").value);
+    alert(a/b);
+    }
